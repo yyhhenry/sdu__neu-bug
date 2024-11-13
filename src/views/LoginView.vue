0363e20e157f66f0ce775e3d27d1bae0480125fe
yyhhenry <yyhhenry@foxmail.com>
2024-11-13 22:14:55 +0800

    添加登录界面的初步实现，包括主题和全局通知组件

---
+++ src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { theme } from '@/utils/theme';
+</script>
+<template>
+  <v-app :theme="theme">
+    <GlobalSnackbar />
+    <v-app-bar title="软件缺陷管理系统"></v-app-bar>
+    <v-main>
+      <v-container> 登录界面尚在开发中 </v-container>
+    </v-main>
+  </v-app>
+</template>
