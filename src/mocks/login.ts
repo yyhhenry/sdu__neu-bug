469523e17e5c36624338ae91d63dfa56e9671175
yyhhenry <yyhhenry@foxmail.com>
2024-11-14 18:17:22 +0800

    feat: login

--- src/mocks/login.ts
+++ src/mocks/login.ts
@@ -1,14 +1,23 @@
 import { http, HttpResponse, RequestHandler } from 'msw';
 
+const expireTime = 1000 * 60 * 5;
+
 export const loginHandlers: RequestHandler[] = [
   http.post('/api/login', () =>
     HttpResponse.json({
       token: {
         accessToken: 'access_token',
         refreshToken: 'refresh_token',
-        expireAt: Date.now() + 1000 * 60 * 60 * 24,
+        expireAt: Date.now() + expireTime,
       },
       role: 'admin',
+    }),
+  ),
+  http.post('/api/refresh', () =>
+    HttpResponse.json({
+      accessToken: 'access_token',
+      refreshToken: 'refresh_token',
+      expireAt: Date.now() + expireTime,
     }),
   ),
 ];
--- src/utils/account.ts
+++ src/utils/account.ts
@@ -1,6 +1,7 @@
 import { DNumber, DString, literal, struct } from '@yyhhenry/type-guard-map';
 import { useCheckedStorage } from './storage';
 import { computed } from 'vue';
+import { GSnackbar } from './global-snackbar';
 
 export const DMsgRes = struct({
   type: literal('info', 'success', 'error'),
@@ -11,6 +12,7 @@ export const DRole = literal('admin', 'user');
 export const DTokenPair = struct({
   accessToken: DString,
   refreshToken: DString,
+  // expireAt is a timestamp, in milliseconds, representing the time when the **access token** will expire
   expireAt: DNumber,
 });
 
@@ -70,6 +72,63 @@ export async function loginApi(req: LoginReq) {
   };
   return accountStorage.value;
 }
+/**
+ * Refresh access token with refresh token.
+ * Do nothing if not logged in or access token is not expired in 1 minute.
+ */
+export async function refreshTokenApi() {
+  if (
+    accountStorage.value === null ||
+    accountStorage.value.token.expireAt >= Date.now() + 1000 * 60
+  ) {
+    return;
+  }
+  const res: unknown = await fetch('/api/refresh', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      refreshToken: accountStorage.value.token.refreshToken,
+    }),
+  }).then((res) => res.json());
+  if (DMsgRes.guard(res)) {
+    // If refresh failed, clear the accountStorage
+    accountStorage.value = null;
+    GSnackbar.error(res.msg);
+    // But do not throw error, the pending request may still be valid
+    return;
+  }
+  const token = DTokenPair.validate(res).unwrap();
+  accountStorage.value.token = token;
+}
+/**
+ * Get Authorization header for API request
+ * @example
+ * ```ts
+ * const res = await fetch('/api/secret', {
+ *   headers: {
+ *     ...await getAuthHeader(),
+ *     'Content-Type': 'application/json',
+ *   },
+ *   method: 'POST',
+ *   body: "{}",
+ * });
+ * ```
+ */
+export async function getAuthHeader() {
+  await refreshTokenApi();
+  if (accountStorage.value === null) {
+    return {};
+  }
+  return {
+    Authorization: `Bearer ${accountStorage.value.token.accessToken}`,
+  };
+}
+/**
+ * Logout.
+ * Actually, it just clear the accountStorage.
+ */
 export function logoutApi() {
   accountStorage.value = null;
 }
